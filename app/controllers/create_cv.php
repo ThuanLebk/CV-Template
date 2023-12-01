3865f1ddfbcf7257d9503e38317d1db0a31cce4c
Thuận Lê Đức <tomle@TomLe.local>
2023-12-01 23:26:45 +0700

    Small update in header title and checkLogin session

--- app/controllers/create_cv.php
+++ app/controllers/create_cv.php
@@ -2,6 +2,7 @@
 
 class create_cv extends Controller {
     public function index() {
+        session_start();
         $this->checkLogin();
         $this->view('header', ['page_title' => 'Create CV']);
     }
--- app/views/header.php
+++ app/views/header.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo isset($_SESSION['logged_in']) ? "OK" : "NULL"; ?></title>
+    <title><?php echo $data['page_title']; ?></title>
     <link rel="icon" href="./assets/CV-icon.png" type="image/x-icon" />    
     <link rel="stylesheet" href="./css/style-home.css">
     
